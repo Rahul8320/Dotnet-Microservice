ed3dbb9e6fef6a3bcf2ae7a246ad4b3d72f0206a
Rahul Dey <rahuldey832000@gmail.com>
2023-10-02 19:08:40 +0530

    Implement Create Coupon

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,7 @@
 using Mango.Web.Service.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Mango.Web.Controllers
 {
@@ -14,6 +15,7 @@ namespace Mango.Web.Controllers
             _couponService = couponService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> CouponIndex()
         {
             List<CouponDto>? list = new();
@@ -27,9 +29,38 @@ namespace Mango.Web.Controllers
             return View(list);
         }
 
+        [HttpGet]
         public IActionResult CouponCreate()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(AddCouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CouponDelete(Guid couponId)
+        {
+			ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+
+			if (response != null && response.IsSuccess)
+			{
+				CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result)!);
+                return View(model);
+			}
+			return NotFound();
         }
     }
 }
