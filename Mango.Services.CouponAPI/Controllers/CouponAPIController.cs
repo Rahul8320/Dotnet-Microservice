1b492e06bad97b7bc34d511576883c33294c98bc
Rahul Dey <rahuldey832000@gmail.com>
2023-10-15 17:49:59 +0530

    Add Product Controller in ProductAPI

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -2,185 +2,188 @@
 using Mango.Services.CouponAPI.Service.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Serilog;
 
-namespace Mango.Services.CouponAPI.Controllers
+namespace Mango.Services.CouponAPI.Controllers;
+
+[Route("api/coupon")]
+[ApiController]
+[Authorize]
+public class CouponAPIController : ControllerBase
 {
-    [Route("api/coupon")]
-    [ApiController]
-    [Authorize]
-    public class CouponAPIController : ControllerBase
+    private readonly ICouponService _couponService;
+    private readonly ResponseDto _response;
+    private readonly ILogger<CouponAPIController> _logger;
+
+    public CouponAPIController(ICouponService couponService, ILogger<CouponAPIController> logger)
     {
-        private readonly ICouponService _couponService;
-        private readonly ResponseDto _response;
+        _couponService = couponService;
+        _response = new ResponseDto();
+        _logger = logger;
+    }
 
-        public CouponAPIController(ICouponService couponService)
+    [HttpGet]
+    public async Task<ResponseDto> Get()
+    {
+        try
         {
-            _couponService = couponService;
-            _response = new ResponseDto();
-        }
-
-        [HttpGet]
-        public async Task<ResponseDto> Get()
-        {
-            try
+            IEnumerable<CouponDto> couponList = await _couponService.GetAllCoupons();
+            if(couponList == null)
             {
-                IEnumerable<CouponDto> couponList = await _couponService.GetAllCoupons();
-                if(couponList == null)
-                {
-                    Log.Information("No Coupons found!");
-                    _response.IsSuccess = false;
-                    _response.Message = "No Coupons Found!";
-                }
-                else
-                {
-                    Log.Information("Get Coupon List: {@objList}", couponList);
-                    _response.Result = couponList;
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.Message, ex);
+                _logger.LogInformation("No Coupons found!");
                 _response.IsSuccess = false;
-                _response.Message = ex.Message;
+                _response.Message = "No Coupons Found!";
             }
-            return _response;
-        }
-
-        [HttpGet]
-        [Route("{id:Guid}")]
-        public async Task<ResponseDto> Get(Guid id) {
-            try
+            else
             {
-                CouponDto coupon = await _couponService.GetCouponById(id);
-                if(coupon == null)
-                {
-                    _response.IsSuccess = false;
-                    Log.Information($"No Coupon Found with id: {id}");
-                    _response.Message = $"No Coupon Found with id: {id}";
-                }
-                Log.Information("Get Coupon: {@objList}", coupon);
+                _logger.LogInformation("Get Coupon List: {@objList}", couponList);
+                _response.Result = couponList;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+
+    [HttpGet]
+    [Route("{id:Guid}")]
+    public async Task<ResponseDto> Get(Guid id) {
+        try
+        {
+            CouponDto coupon = await _couponService.GetCouponById(id);
+            if(coupon == null)
+            {
+                _response.IsSuccess = false;
+                _logger.LogInformation($"No Coupon Found with id: {id}");
+                _response.Message = $"No Coupon Found with id: {id}";
+            }
+            _logger.LogInformation("Get Coupon: {@objList}", coupon);
+            _response.Result = coupon;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+
+    [HttpGet]
+    [Route("GetByCode/{code}")]
+    public async Task<ResponseDto> GetByCode(string code)
+    {
+        try
+        {
+            if(code.Trim().Length <= 3)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Coupon Code is Invalid!";
+                return _response;   
+            }
+
+            CouponDto coupon = await _couponService.GetCouponByCode(code);
+            if(coupon == null)
+            {
+                _response.IsSuccess = false;
+                _logger.LogInformation($"No Coupon Found with code: {code}");
+                _response.Message = $"No Coupon Found with code: {code}";
+            }
+            else
+            {
+                _logger.LogInformation("Get Coupon: {@objList}", coupon);
                 _response.Result = coupon;
             }
-            catch (Exception ex)
-            {
-                Log.Error(ex.Message, ex);
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
-            }
-            return _response;
         }
-
-        [HttpGet]
-        [Route("GetByCode/{code}")]
-        public async Task<ResponseDto> GetByCode(string code)
+        catch (Exception ex)
         {
-            try
-            {
-                if(code.Trim().Length <= 3)
-                {
-                    _response.IsSuccess = false;
-                    _response.Message = "Coupon Code is Invalid!";
-                    return _response;   
-                }
-
-                CouponDto coupon = await _couponService.GetCouponByCode(code);
-                if(coupon == null)
-                {
-                    _response.IsSuccess = false;
-                    Log.Information($"No Coupon Found with code: {code}");
-                    _response.Message = $"No Coupon Found with code: {code}";
-                }
-                else
-                {
-                    Log.Information("Get Coupon: {@objList}", coupon);
-                    _response.Result = coupon;
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.Message, ex);
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
-            }
-            return _response;
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
         }
+        return _response;
+    }
 
-        [HttpPost]
-        [Authorize(Roles = "ADMIN")]
-        public async Task<ResponseDto> Post(AddCouponDto coupon)
+    [HttpPost]
+    [Authorize(Roles = "ADMIN")]
+    public async Task<ResponseDto> Post(AddCouponDto coupon)
+    {
+        try
         {
-            try
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
-                {
-                    var response = await _couponService.CreateCoupon(coupon);
-                    Log.Information($"Coupon Created {response}");
-                    _response.Result = response;
-                }
-                else
-                {
-                    _response.IsSuccess = false;
-                    _response.Result = ModelState;
-                }
+                var response = await _couponService.CreateCoupon(coupon);
+                _logger.LogInformation("Coupon Created {@response}", response);
+                _response.Result = response;
             }
-            catch (Exception ex)
+            else
             {
-                Log.Error(ex.Message, ex);
                 _response.IsSuccess = false;
-                _response.Message = ex.Message;
+                _response.Result = ModelState;
             }
-            return _response;
         }
-
-        [HttpPut]
-        [Authorize(Roles = "ADMIN")]
-        public async Task<ResponseDto> Put(CouponDto coupon)
+        catch (Exception ex)
         {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    var response = await _couponService.UpdateCoupon(coupon);
-                    Log.Information($"Coupon Updated {response}");
-                    _response.Result = response;
-                }
-                else
-                {
-                    _response.IsSuccess = false;
-                    _response.Result = ModelState;
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.Message, ex);
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
-            }
-            return _response;
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
         }
+        return _response;
+    }
 
-        [HttpDelete]
-        [Authorize(Roles = "ADMIN")]
-        [Route("{id:Guid}")]
-        public async Task<ResponseDto> Delete(Guid id)
+    [HttpPut]
+    [Authorize(Roles = "ADMIN")]
+    public async Task<ResponseDto> Put(CouponDto coupon)
+    {
+        try
         {
-            try
+            if (ModelState.IsValid)
             {
-                var response = await _couponService.DeleteCoupon(id);
-                if(response == true)
-                {
-                    _response.Message = $"Coupon with id: {id} Deleted Successfully!";
-                }
-                _response.IsSuccess = response;
+                var response = await _couponService.UpdateCoupon(coupon);
+                _logger.LogInformation("Coupon Updated {@response}",response);
+                _response.Result = response;
             }
-            catch (Exception ex)
+            else
             {
-                Log.Error(ex.Message, ex);
                 _response.IsSuccess = false;
-                _response.Message = ex.Message;
+                _response.Result = ModelState;
             }
-            return _response;
         }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = "ADMIN")]
+    [Route("{id:Guid}")]
+    public async Task<ResponseDto> Delete(Guid id)
+    {
+        try
+        {
+            var response = await _couponService.DeleteCoupon(id);
+
+            if(response == true)
+            {
+                _logger.LogInformation("Coupon with id: {@id} Deleted Successfully!", id);
+                _response.Message = $"Coupon with id: {id} Deleted Successfully!";
+            }
+
+            _response.IsSuccess = response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
     }
 }
---
+++ Mango.Services.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,162 @@
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Services.ProductAPI.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers;
+
+[Route("api/product")]
+[ApiController]
+[Authorize]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+    private readonly ResponseDto _response;
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(IProductService productService, ResponseDto response, ILogger<ProductController> logger)
+    {
+        _productService = productService;
+        _response = response;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ResponseDto> Get()
+    {
+        try
+        {
+            IEnumerable<ProductDto> productList = await _productService.GetAllProducts();
+            if (productList == null)
+            {
+                _logger.LogInformation("No Products found!");
+                _response.IsSuccess = false;
+                _response.Message = "No Products Found!";
+            }
+            else
+            {
+                _logger.LogInformation("Get products List: {@objList}", productList);
+                _response.Result = productList;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+
+    [HttpGet]
+    [Route("{id:Guid}")]
+    public async Task<ResponseDto> Get(Guid id)
+    {
+        try
+        {
+            ProductDto product = await _productService.GetProductById(id);
+            if (product == null)
+            {
+                _response.IsSuccess = false;
+                _logger.LogInformation("No Product Found with id: {@id}", id);
+                _response.Message = $"No Product Found with id: {id}";
+            }
+            _logger.LogInformation("Get Product: {@objList}", product);
+            _response.Result = product;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "ADMIN")]
+    public async Task<ResponseDto> Post(AddProductDto product)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(product);
+                _logger.LogInformation("Product Created {@response}", response);
+                _response.Result = response;
+            }
+            else
+            {
+                _response.IsSuccess = false;
+                _response.Result = ModelState;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+
+    [HttpPut]
+    [Authorize(Roles = "ADMIN")]
+    public async Task<ResponseDto> Put(ProductDto product)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(product);
+                _logger.LogInformation("Product Updated {@response}", response);
+                _response.Result = response;
+            }
+            else
+            {
+                _response.IsSuccess = false;
+                _response.Result = ModelState;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = "ADMIN")]
+    public async Task<ResponseDto> Delete(Guid id)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.DeleteProduct(id);
+
+                if(response == true)
+                {
+                    _logger.LogInformation("Product with id: {@id} Deleted Successfully!", id);
+                    _response.Message = $"Product with id: {id} Deleted Successfully!";
+                }
+
+                _response.Result = response;
+            }
+            else
+            {
+                _response.IsSuccess = false;
+                _response.Result = ModelState;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex);
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+        return _response;
+    }
+}
